988a52c80e93ba0c1d239b4561242123ac876b34
Prashant Mahajan <PrashantMahajan.ma@gmail.com>
2019-05-13 11:22:59 +0530

    Nested Loop Exercise

---
+++ tutorial-012-forLoops.py
@@ -0,0 +1,25 @@
+"""
+/*
+ * 
+ * Prashant CONFIDENTIAL
+ * __________________
+ * 
+ *  Prashant All Rights Reserved.
+ * 
+ * NOTICE:  All information contained herein is, and remains the property
+ * of Prashant and its suppliers, if any.  The intellectual and technical
+ * concepts contained herein are proprietary to Prashant and its suppliers
+ * and may be covered by U.S. and Foreign Patents, patents in process, and are
+ * protected by trade secret or copyright law. Dissemination of this information
+ * or reproduction of this material is strictly forbidden unless prior written
+ * permission is obtained from Prashant.
+ */
+/**
+ * @author prashantmahajan
+ */
+ """
+total = 0;
+price = range(200, 500, 5);
+for itr in price:
+    total += itr;
+print(total)
---
+++ tutorial-013-nestedLoops.py
@@ -0,0 +1,38 @@
+"""
+/*
+ * 
+ * Prashant CONFIDENTIAL
+ * __________________
+ * 
+ *  Prashant All Rights Reserved.
+ * 
+ * NOTICE:  All information contained herein is, and remains the property
+ * of Prashant and its suppliers, if any.  The intellectual and technical
+ * concepts contained herein are proprietary to Prashant and its suppliers
+ * and may be covered by U.S. and Foreign Patents, patents in process, and are
+ * protected by trade secret or copyright law. Dissemination of this information
+ * or reproduction of this material is strictly forbidden unless prior written
+ * permission is obtained from Prashant.
+ */
+/**
+ * @author prashantmahajan
+ */
+ """
+# https://youtu.be/_uQrJ0TkZlc?t=6474
+# Draw F
+
+characters = [5, 2, 5, 2, 2]
+
+for i in characters:
+    print("x" * i);
+
+str = "";
+j = 0;
+print("--------------");
+for i in characters:
+    str = "";
+    j = 0;
+    while j < i:
+        str += "x";
+        j += 1;
+    print(str)
